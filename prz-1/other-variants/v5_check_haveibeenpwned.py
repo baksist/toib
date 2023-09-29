efa47e56638e1ac89ba4fcefba393d9281fa9d4d
Matvey Serdyukov <matvey.serdukov@gmail.com>
2023-09-29 21:03:37 +0300

    some more reorganization

--- prz-1/other-variants/v5_check_haveibeenpwned.py
+++ prz-1/other-variants/v5_check_haveibeenpwned.py
@@ -1,7 +1,8 @@
 import requests
 import hashlib
 
-def api_call(password):
+def api_call(credential):
+    [ username, password ] = credential.split(',')
     pass_hash = hashlib.sha1(password.encode()).hexdigest().upper()
     hash_prefix = pass_hash[:5]
     r = requests.get(f'https://api.pwnedpasswords.com/range/{hash_prefix}')
@@ -10,13 +11,13 @@ def api_call(password):
         hash_suffix = line.split(':')[0]
         hash_counter = line.split(':')[1]
         if pass_hash == hash_prefix + hash_suffix:
-            result = f'Hit! Password {password} been pwned {hash_counter} times'
+            result = f'Hit! Password {password} of user {username} been pwned {hash_counter} times'
             return result
-    result = f'Password {password} hasn\'t been pwned'
+    result = f'Password {password} hasn\'t been pwned, user {username} is safe'
     return result
 
-with open('./passwords.lst') as passlist:
-    passwords = passlist.read().splitlines()
+with open('./credentials.lst') as file:
+    creds = file.read().splitlines()
     
-for current_pass in passwords:
-    print(api_call(current_pass))
+for current_user in creds:
+    print(api_call(current_user))
